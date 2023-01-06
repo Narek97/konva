c7f17b4ff13c80a0688c402ca1df9ba5c05690d4
Narek97 <narek.babayan.97.27@gmail.com>
2023-01-07 00:14:23 +0400

    Transformer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React, { MouseEvent, useRef, useState } from "react";
-import { Layer, Stage } from "react-konva";
+import React, { LegacyRef, MouseEvent, useRef, useState } from "react";
+import { Layer, Rect, Stage, Transformer } from "react-konva";
+import Konva from "konva";
 import Paint from "../src/assests/paint.svg";
+
+declare global {
+  interface Window {
+    Konva: typeof Konva;
+  }
+}
 
 const App = () => {
   const layerRef = useRef<any>(null);
   const [currentShape, setCurrentShape] = useState<string | null>(null);
   const [selectIcon, setSelectIcon] = useState<string | null>(null);
+
+  const trRef = useRef<any>(null);
+  const selectionRectRef = useRef<any>(null);
+  const selection = useRef<{
+    visible: boolean;
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+  }>({
+    visible: false,
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0,
+  });
 
   const onAddShape = (shape: string, src?: string) => {
     setCurrentShape(shape);
@@ -15,6 +38,69 @@ const App = () => {
   const onAddIcon = (event: MouseEvent<HTMLImageElement>) => {
     setSelectIcon((event.target as HTMLImageElement).src);
     setCurrentShape(null);
+  };
+
+  const updateSelectionRect = () => {
+    const node = selectionRectRef.current;
+    node.setAttrs({
+      visible: selection.current.visible,
+      x: Math.min(selection.current.x1, selection.current.x2),
+      y: Math.min(selection.current.y1, selection.current.y2),
+      width: Math.abs(selection.current.x1 - selection.current.x2),
+      height: Math.abs(selection.current.y1 - selection.current.y2),
+      fill: "rgba(0, 161, 255, 0.3)",
+    });
+    node.getLayer().batchDraw();
+  };
+
+  const onMouseDown = (e: Konva.KonvaEventObject<any>) => {
+    const pos = e.target.getStage()?.getPointerPosition();
+    selection.current.visible = true;
+    selection.current.x1 = pos?.x || 0;
+    selection.current.y1 = pos?.y || 0;
+    selection.current.x2 = pos?.x || 0;
+    selection.current.y2 = pos?.y || 0;
+    updateSelectionRect();
+  };
+
+  const onMouseMove = (e: Konva.KonvaEventObject<any>) => {
+    const pos = e.target.getStage()?.getPointerPosition();
+    selection.current.x2 = pos?.x || 0;
+    selection.current.y2 = pos?.y || 0;
+    updateSelectionRect();
+  };
+
+  const onMouseUp = () => {
+    if (!selection.current.visible) {
+      return;
+    }
+    const selBox = selectionRectRef.current.getClientRect();
+    const elements: any = [];
+    [
+      ...layerRef.current.find(".square"),
+      ...layerRef.current.find(".triangle"),
+      ...layerRef.current.find(".ellipse"),
+      ...layerRef.current.find(".star"),
+      ...layerRef.current.find(".roundSquare"),
+      // ...layerRef.current.find(".paint"),
+      // ...layerRef.current.find(".icon"),
+      // ...layerRef.current.find(".arrow"),
+    ].forEach((elementNode: any) => {
+      const elBox = elementNode.getClientRect();
+      if (Konva.Util.haveIntersection(selBox, elBox)) {
+        elements.push(elementNode);
+      }
+    });
+    trRef.current.nodes(elements);
+    selection.current.visible = false;
+    updateSelectionRect();
+  };
+
+  const onTouchStart = (e: Konva.KonvaEventObject<any>) => {
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      trRef.current.nodes([]);
+    }
   };
 
   return (
@@ -32,10 +118,10 @@ const App = () => {
         <Stage
           width={window.innerWidth - 40}
           height={window.innerHeight - 140}
-          // onMouseDown={}
-          // onMouseUp={}
-          // onMouseMove={}
-          // onTouchStart={}
+          onMouseDown={onMouseDown}
+          onMouseMove={onMouseMove}
+          onMouseUp={onMouseUp}
+          onTouchStart={onTouchStart}
           // onClick={}
 
           style={{
@@ -50,7 +136,21 @@ const App = () => {
           className={`board`}
           id={"stage"}
         >
-          <Layer ref={layerRef}></Layer>
+          <Layer ref={layerRef}>
+            <Transformer
+              resizeEnabled={true}
+              rotateEnabled={true}
+              ref={trRef}
+              boundBoxFunc={(oldBox, newBox) => {
+                if (newBox.width < 30 || newBox.height < 30) {
+                  return oldBox;
+                }
+                return newBox;
+              }}
+            />
+
+            <Rect fill="rgba(0,0,255,0.5)" ref={selectionRectRef} />
+          </Layer>
         </Stage>
       </div>
     </div>
